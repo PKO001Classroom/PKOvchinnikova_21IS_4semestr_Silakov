8ac1fc66be6fbffb7f83a2f5cc8ad34774f34cbc
Grozio <165637920+Grozard@users.noreply.github.com>
2026-02-16 14:19:23 +0300

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Создание виртуального окружения...
+python -m venv venv
+echo Активация окружения...
+call venv\Scripts\activate
+echo Установка зависимостей из requirements.txt...
+pip install -r requirements.txt
+echo Готово!
+pause
